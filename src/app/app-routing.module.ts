059a03be6c574b201e5ae535834b6b92419045bb
Bhavay Anand <bhavayanandcse@gmail.com>
2019-05-19 00:32:10 +0530

    blank route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,10 @@ import { SearchComponent } from "./search/search.component";
 import { AllreposComponent } from './allrepos/allrepos.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'search', pathMatch: 'full'},
   {path: 'search', component: SearchComponent},
-  {path: 'repos/:handle', component: AllreposComponent}
+  {path: 'repos/:handle', component: AllreposComponent},
+  {path: '*', redirectTo: 'search', pathMatch: 'full'}
 ];
 
 @NgModule({
